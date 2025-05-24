4b04270d1a78026f9b19066aa16c3fd1921ce3e1
johnndivo <johnndivo@gmail.com>
2025-05-25 02:41:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 # Placeholder for setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='fleet_billing',
+    version='0.0.1',
+    description='Fleet billing automation for ERPNext',
+    author='John Ndivo',
+    author_email='johnndivo@outlook.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
